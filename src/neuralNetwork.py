71afe1acbdd7ee265983397e237b514482f04f73
YuelongLi <cloudnest.liyuelong@gmail.com>
2017-12-18 14:01:36 -0500

    Add foward propagation and network initialization functions

--- src/neuralNetwork.py
+++ src/neuralNetwork.py
@@ -1,1 +1,21 @@
+import numpy as np
 
+def initializeNetwork(layerSizes = [1,2,3,4,1]):
+    l = len(layerSizes)
+    parameters = {}
+    for i in range(1,l):
+        parameters['W'+str(i)] = np.random.randn(layerSizes[i],layerSizes[i-1])*0.1
+        parameters['b'+str(i)] = np.empty((i,1))
+    return parameters
+
+def forwardProp(X, parameters):
+    As = {}
+    A = X
+    l = len(parameters)//2
+    for i in range(1, l):
+        A = np.dot(parameters['W'+str(i)],A)
+        As['A'+str(i)] = A
+    return As
+
+parameters = initializeNetwork()
+forwardProp(np.array([[1,2,3,1]]),parameters)
